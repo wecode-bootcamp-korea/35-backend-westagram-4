635547b397d8d49c8f0d01bace0873175dfe4f5b
eunhyung <ooooo5h91@gmail.com>
2022-07-08 08:37:26 +0900

    Add: created users app and completed view for sign-up

--- students/eunhyung/users/models.py
+++ students/eunhyung/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length=10)
-    email        = models.CharField(max_length=30)
-    password     = models.CharField(max_length=10)
+    name         = models.CharField(max_length=50)
+    email        = models.CharField(max_length=50)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
 
     class Meta:
