ee45d7509d0a24a13ff47a45fdd4d807fe4f09b2
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-07-07 19:33:10 +0900

    models modified

--- students/jeongyong/users/models.py
+++ students/jeongyong/users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    user_name     = models.CharField(max_length=50)
-    user_email    = models.CharField(max_length=200 , unique=True)
-    user_password = models.CharField(max_length=200)
-    user_contact  = models.CharField(max_length=150)
+    name       = models.CharField(max_length=50)
+    email      = models.CharField(max_length=200 , unique=True)
+    password   = models.CharField(max_length=200)
+    contact    = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True , null=True)
+    updated_at = models.DateTimeField(auto_now=True , null=True)
 
     class Meta:
         db_table = 'users'
