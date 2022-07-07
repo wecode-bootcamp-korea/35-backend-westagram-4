95a97b04d9dd50a3fe5766c60af3c1b0dece37af
xoxowo <1qq11212@gmail.com>
2022-07-07 16:42:03 +0900

    Add : yujeong/user/models.py update

--- students/yujeong/users/models.py
+++ students/yujeong/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model) :
     user_name         = models.CharField(max_length = 20)
     user_email        = models.EmailField(max_length = 45)
     user_id           = models.CharField(max_length = 50)
-    user_pw           = models.CharField(max_length=20)
+    user_pw           = models.CharField(max_length = 20)
     user_phone_number = models.CharField(max_length = 50)
     
     class Meta :
