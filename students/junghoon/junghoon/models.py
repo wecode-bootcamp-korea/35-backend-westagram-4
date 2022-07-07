0ce62409d29fd620804370a72d6f7df8c09e6be1
fkelfk <fkelfk09@gmail.com>
2022-07-07 17:42:52 +0900

    모델 마이그레이션 및 속성 추가

--- students/junghoon/junghoon/models.py
+++ students/junghoon/junghoon/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    name     = models.CharField(max_length=45)
-    email    = models.CharField(max_length=200)
-    password = models.TextField()
-    contact  = models.CharFiled(max_length=45)
+    name       = models.CharField(max_length=200)
+    email      = models.CharField(max_length=200, help_text='최대 200글자 내로 입력하세요.', unique=True)
+    password   = models.TextField(null=True)
+    contact    = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-	db_table = 'Users'
-	
+        db_table = 'Users'
+
     def __str__(self):
-	return self.name
+        return self.name
