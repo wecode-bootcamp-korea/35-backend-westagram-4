faa848f189e9c3e4df85d4ea9b9c4d272079fff2
fkelfk <fkelfk09@gmail.com>
2022-07-08 11:05:48 +0900

    contact 속성 수정

--- students/junghoon/user/models.py
+++ students/junghoon/user/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name       = models.CharField(max_length=50)
     email      = models.CharField(max_length=50, unique=True)
     password   = models.CharField(max_length=200)
-    contact    = models.CharField(max_length=50, blank=True, null=True)
+    contact    = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
