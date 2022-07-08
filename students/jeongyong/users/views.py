fa205f2d690872b7a6815aa622b345e1e6d2c114
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-07-08 15:36:14 +0900

    modifed signup view

--- students/jeongyong/users/views.py
+++ students/jeongyong/users/views.py
@@ -17,13 +17,13 @@ class SignUpView(View):
             password = data['password']
        
             if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$' , email) :
-                return JsonResponse({'messasge':'이메일 형식에 맞지 않습니다.'}, status=400)
+                return JsonResponse({'messasge':'Invalid Email'}, status=400)
         
             if not re.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$" , password) :
-                return JsonResponse({'messasge':'비밀번호 형식에 맞지 않습니다.'}, status=400)
+                return JsonResponse({'messasge':'Invalid Password'}, status=400)
 
             if User.objects.filter(email=email).exists() :
-                return JsonResponse({'message':'존재하는 이메일 입니다.'}, status=400)
+                return JsonResponse({'message':'Email is exist'}, status=400)
 
             User.objects.create(
                 name     = name,
@@ -32,7 +32,7 @@ class SignUpView(View):
                 contact  = contact,
             )    
       
-            return JsonResponse({'messasge':'회원가입 완료'}, status=201)
+            return JsonResponse({'messasge':'Success'}, status=201)
         
         except KeyError :
             return JsonResponse({'messasge':'KEY ERROR'}, status=400)    
