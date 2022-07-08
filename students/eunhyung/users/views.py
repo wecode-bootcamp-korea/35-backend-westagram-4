93bf8e05d91b0f79d40b5ece823f127c7a9804e2
eunhyung <ooooo5h91@gmail.com>
2022-07-08 16:17:51 +0900

    modify signupview

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -11,27 +11,39 @@ class SignUpView(View):
         
         data = json.loads(request.body)
         
-        valid_email    = re.compile('^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$')
-        valid_password = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
-        
-        if data['email'] == '' or data['password'] == '':
+        try:
+            name         = data['name']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+            
+            REGEX_EMAIL    = '^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+            
+            if not re.match(REGEX_EMAIL, data['email']):
+                return JsonResponse({'message':'Email is invalid.'}, status=400)
+            
+            if not re.match(REGEX_PASSWORD, data['password']):
+                return JsonResponse({'message':'Password is invalid.'}, status=400)
+            
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'Email already in use.'}, status=400)
+         
+            User.objects.create(
+                name          = name,
+                email         = email,
+                password      = password,
+                phone_number  = phone_number
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+            
+        except:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        if not valid_email.match(data['email']):
-            return JsonResponse({'message':'Email is invalid.'}, status=400)
-         
-        if not valid_password.match(data['password']):
-            return JsonResponse({'message':'Password is invalid.'}, status=400)
+    
         
-        if User.objects.filter(email=data['email']).exists():
-            return JsonResponse({'message':'Email already in use.'}, status=400)
-         
-        User.objects.create(
-            name          = data['name'],
-            email         = data['email'],
-            password      = data['password'],
-            phone_number  = data['phone_number'],
-        )
+    
         
-        return JsonResponse({'message':'SUCCESS'}, status=201)
+       
+        
+        
         
