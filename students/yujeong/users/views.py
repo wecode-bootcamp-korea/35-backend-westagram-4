fed57a98ee5e6949d2ab59cb16b54264d4ac1797
xoxowo <1qq11212@gmail.com>
2022-07-08 16:57:58 +0900

    Add :view.py in signup func modified

--- students/yujeong/users/views.py
+++ students/yujeong/users/views.py
@@ -11,22 +11,20 @@ class SignUpView(View):
         data = json.loads(request.body) 
         email_form = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
         password_form = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
-        
+
         try :
-            data['password'] and data['email'] == None
-        except KeyError :
-            return JsonResponse({"message": "KEY_ERROR"}, status = 400)   
+            if data['password'] and data['email'] == None :
+                return JsonResponse({"message": "Email or Password is Empyt"}, status = 400)   
 
-        if User.objects.filter(email=data['email']).exists(): # 중복된 이메일 검사
-            return JsonResponse({"message": "Already registered Email"}, status = 400)   
+            if User.objects.filter(email=data['email']).exists(): # 중복된 이메일 검사
+                return JsonResponse({"message": "Already registered Email"}, status = 400)   
 
-        if not email_form.match(data['email']) :
-            return JsonResponse({"message": "Invalid email format"}, status = 400)   
+            if not email_form.match(data['email']) :
+                return JsonResponse({"message": "Invalid email format"}, status = 400)   
 
-        if not password_form.match(data['password']):
-            return JsonResponse({"message": "Invalid password format"}, status = 400)
-        
-        else:
+            if not password_form.match(data['password']):
+                return JsonResponse({"message": "Invalid password format"}, status = 400)
+            
             User.objects.create(
                 name         = data['name'],
                 email        = data['email'],
@@ -35,3 +33,6 @@ class SignUpView(View):
                 phone_number = data['phone_number'],
             )
             return JsonResponse({"message": "SUCCESS"}, status = 201)
+            
+        except KeyError :
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)       
