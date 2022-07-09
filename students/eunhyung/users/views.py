bed2d8f28d40adc37b61c877afc87bf2fb2108e7
eunhyung <ooooo5h91@gmail.com>
2022-07-09 09:23:31 +0900

    잘못된 이메일만 입력시 KEY_ERROR로 빠지지않고 INVALID_USER로 빠지는 이유

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -46,15 +46,18 @@ class SignInView(View):
         try:
             data = json.loads(request.body)
             email_exist_user = User.objects.get(email=data['email'])
-            
-            print('비밀번호가 안들어왔다')
-            
-            if data['password'] != email_exist_user.password:
+                        
+            if data['password'] == email_exist_user.password:
+                print('아이디와 비밀번호 모두 일치')
+                return JsonResponse({'message':'SUCCESS'}, status=200)
+            else:
+                print('아이디는 있지만 비밀번호 불일치')
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
-            
-            return JsonResponse({'message':'SUCCESS'}, status=200)
                   
         except KeyError:
+            print('아이디나 비밀번호 입력 안들어옴')
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
         except User.DoesNotExist:
+            print('없는 사용자 이메일 입력시')
+            # 이메일만 입력시 KEY_ERROR로 빠져야하는데 48번줄에서 email이 불일치가 되서 이쪽으로 빠지게된다.
             return JsonResponse({'message':'INVALID_USER'}, status=401)
