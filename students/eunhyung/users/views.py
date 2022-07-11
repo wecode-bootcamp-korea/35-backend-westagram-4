bb84ef8954e4ac3a3ab51d1888d058da5f466e37
eunhyung <ooooo5h91@gmail.com>
2022-07-11 17:50:57 +0900

    선언한 변수 사용

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -1,6 +1,6 @@
+import bcrypt
 import json
 import re
-import bcrypt
 
 from django.http  import JsonResponse
 from django.views import View
@@ -21,13 +21,13 @@ class SignUpView(View):
             REGEX_EMAIL    = '^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$'
             REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             
-            if not re.match(REGEX_EMAIL, data['email']):
+            if not re.match(REGEX_EMAIL, email):
                 return JsonResponse({'message':'Email is invalid.'}, status=400)
             
             if not re.match(REGEX_PASSWORD, data['password']):
                 return JsonResponse({'message':'Password is invalid.'}, status=400)
             
-            if User.objects.filter(email=data['email']).exists():
+            if User.objects.filter(email=email).exists():
                 return JsonResponse({'message':'Email already in use.'}, status=400)
          
             User.objects.create(
