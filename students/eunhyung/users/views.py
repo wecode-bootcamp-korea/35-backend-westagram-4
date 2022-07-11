431a5417f741ad228c12c5d5d3ceb87121def903
eunhyung <ooooo5h91@gmail.com>
2022-07-11 17:02:56 +0900

    불필요한 주석 삭제

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -15,12 +15,10 @@ class SignUpView(View):
         try:
             name         = data['name']
             email        = data['email']
-            # password     = data['password']
-            # salt 저장
-            salt = bcrypt.gensalt()
-            # 암호화
-            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), salt)
             phone_number = data['phone_number']
+            
+            salt            = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), salt)
             
             REGEX_EMAIL    = '^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$'
             REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
@@ -34,13 +32,11 @@ class SignUpView(View):
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message':'Email already in use.'}, status=400)
          
-            print(hashed_password)
             User.objects.create(
-                name          = name,
-                email         = email,
-                # password      = hashed_password,    # 그냥 이렇게 넣으면 bytes형태로 들어가기 때문에 string으로 바꿔주는 디코딩작업이 필요하다
-                password = hashed_password.decode('utf-8'),
-                phone_number  = phone_number
+                name         = name,
+                email        = email,
+                password     = hashed_password.decode('utf-8'),
+                phone_number = phone_number
             )
             return JsonResponse({'message':'SUCCESS'}, status=201)
             
