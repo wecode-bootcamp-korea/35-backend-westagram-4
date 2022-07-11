9f846c2dd4b1bc4e8cae49e38ebb0fa477850de7
xoxowo <1qq11212@gmail.com>
2022-07-11 16:58:45 +0900

    Modified :users/views.py SinInView class

--- students/yujeong/users/views.py
+++ students/yujeong/users/views.py
@@ -43,11 +43,8 @@ class SignInView(View):
             if not User.objects.filter(email=data['email']).exists() :
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)   
 
-            if User.objects.filter(email=data['email']):
-                if User.objects.filter(password=data['password']):
-                    return JsonResponse({"message": "SUCCESS"}, status = 200) 
-                else:
-                    return JsonResponse({"message": "INVALID_USER"}, status = 401) 
+            if not User.objects.filter(email=data['email'], password=data['password']):
+                return JsonResponse({"message": "INVALID_USER"}, status = 401)
 
         except KeyError :
             return JsonResponse({"message": "KEY_ERROR"}, status = 400)
