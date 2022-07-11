99d55821a37647b13535f50aaaa657a2e1fcc189
eunhyung <ooooo5h91@gmail.com>
2022-07-11 23:18:57 +0900

    주석 삭제

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -1,13 +1,13 @@
 import bcrypt
 import json
+import jwt 
 import re
-import jwt  # 패키지 설치는 pyjwt지만 import 모듈 이름은 jwt
 
 from django.http  import JsonResponse
 from django.views import View
 
-from users.models import User
 from my_settings import SECRET_KEY
+from users.models import User
 
 class SignUpView(View):
     def post(self, request):
@@ -53,18 +53,14 @@ class SignInView(View):
             password = data['password']
             
             email_exist_user = User.objects.get(email=email)
-                    
-            # 해시화된 비밀번호와 들어온 비밀번호 해시한 결과값이 같은지 비교하기
-            # if password != email_exist_user.password:
-            # DB에 저장된 string도 인코딩 , 클라이언트가 입력한 password string도 인코딩해서 비교해야함
-            # 그 값이 false라면 틀리단 이야기
+            
             if not bcrypt.checkpw(password.encode('utf-8'), email_exist_user.password.encode('utf-8')):
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
             
-            # 코드가 이쪽으로 넘어오면, 비밀번호가 일치한단 이야기
-            # 그러면 JWT발급하고, 응답에 담아 보내기
-            SECRET = SECRET_KEY
+            SECRET     = SECRET_KEY
+            
             user_token = jwt.encode({'user_id':email_exist_user.id}, SECRET, algorithm='HS256')
+            
             return JsonResponse({'token':user_token}, status=200)
                   
         except KeyError:
