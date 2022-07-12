8ef02cc02d9d495257036ba7f9ac638b074ea2fd
eunhyung <ooooo5h91@gmail.com>
2022-07-12 17:26:44 +0900

    my_settings.py와의 의존성 싹뚝

--- students/eunhyung/users/views.py
+++ students/eunhyung/users/views.py
@@ -6,7 +6,19 @@ import jwt
 from django.http  import JsonResponse
 from django.views import View
 
-from my_settings  import ALGORITHM, SECRET_KEY
+# from my_settings  import ALGORITHM, SECRET_KEY   
+# ALGORITHM, SECRET_KEY와 같은 환경변수를 my_settings.py파일로 관리하고 있다.
+# my_settings.py파일은 환경변수 관리로서의 역할만 하는 파일이다. 
+# 따라서 import해서 사용하면 my_settings.py파일에 의존하게 되므로, 의존성이 생긴다.
+
+# 만약 환경변수를 my_settings.py파일이 아니라 다른 방법으로 관리한다면?
+# 만약 파일 이름이 my_settings가 아니라 your_settings가 된다면?
+# import한 부분의 코드들을 모두 바까줘야한다.
+
+# 그러니까 my_settings에서 불러오지말고, 수정되어도 아무 지장이 없도록
+from django.conf import settings
+# 를 사용해서 가져오자.
+
 from users.models import User
 
 class SignUpView(View):
@@ -59,7 +71,7 @@ class SignInView(View):
             if not bcrypt.checkpw(password.encode('utf-8'), email_exist_user.password.encode('utf-8')):
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
             
-            user_token = jwt.encode({'user_id':email_exist_user.id}, SECRET_KEY, ALGORITHM)
+            user_token = jwt.encode({'user_id':email_exist_user.id}, settings.SECRET_KEY, settings.ALGORITHM)
             
             return JsonResponse({'token':user_token}, status=200)
                   
