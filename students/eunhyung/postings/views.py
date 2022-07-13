b5fbac0b5b083b02172ed3f07cf42f712b271d83
eunhyung <ooooo5h91@gmail.com>
2022-07-14 08:55:24 +0900

    게시글 발행 post 초안 작성

--- students/eunhyung/postings/views.py
+++ students/eunhyung/postings/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+from postings.models import Posting
+
+class PostingView(View):
+    def post(self, request):
+        """
+        게시글 발행 기능
+        
+        # user_id가 존재하는지 확인
+        """
+        data = json.loads(request.body)
+        
+        try:
+            user    = data['user_id']
+            image   = data['image']
+            content = data['content']
+            
+            if not User.objects.get(user_id=user):
+                return JsonResponse({'message':'User is not exist'}, status=401)
+        
+            Posting.objects.create(
+                user    = user,
+                image   = image,
+                content = content
+            )
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+            
